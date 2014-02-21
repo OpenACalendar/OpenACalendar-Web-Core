1d3e99a379e39fdc38398286129c54404b23f7b3
James <james@jarofgreen.co.uk>
2014-02-21 09:04:15 +0000

    Bug fix; on update area parent, need to clear cache

--- extension.Core/php/repositories/AreaRepository.php
+++ extension.Core/php/repositories/AreaRepository.php
@@ -167,6 +167,43 @@ class AreaRepository {
 			$DB->beginTransaction();
 
 			$stat = $DB->prepare("UPDATE area_information  SET ".
+					"title=:title,description=:description, is_deleted='0'".
+					"WHERE id=:id");
+			$stat->execute(array(
+					'id'=>$area->getId(),
+					'title'=>$area->getTitle(),
+					'description'=>$area->getDescription(),
+					'parent_area_id'=>$area->getParentAreaId(),
+				));
+			
+			$stat = $DB->prepare("INSERT INTO area_history (area_id,  title,description,country_id,parent_area_id,user_account_id  , created_at) VALUES ".
+					"(:area_id,  :title,:description,:country_id,:parent_area_id,:user_account_id, :created_at)");
+			$stat->execute(array(
+					'area_id'=>$area->getId(),
+					'title'=>substr($area->getTitle(),0,VARCHAR_COLUMN_LENGTH_USED),
+					'description'=>$area->getDescription(),
+					'country_id'=>$area->getCountryId(),
+					'parent_area_id'=>$area->getParentAreaId(),
+					'user_account_id'=>$creator->getId(),				
+					'created_at'=>\TimeSource::getFormattedForDataBase(),
+				));
+			
+			
+			$DB->commit();
+		} catch (Exception $e) {
+			$DB->rollBack();
+		}
+	}
+	
+	public function editParentArea(AreaModel $area, UserAccountModel $creator) {
+		global $DB;
+		if ($area->getIsDeleted()) {
+			throw new \Exception("Can't edit deleted area!");
+		}
+		try {
+			$DB->beginTransaction();
+
+			$stat = $DB->prepare("UPDATE area_information  SET ".
 					"title=:title,description=:description, is_deleted='0', parent_area_id=:parent_area_id ".
 					"WHERE id=:id");
 			$stat->execute(array(
@@ -188,6 +225,10 @@ class AreaRepository {
 					'created_at'=>\TimeSource::getFormattedForDataBase(),
 				));
 			
+			// new must clear caches
+			// TODO clear for this area only - for now clear all.
+			$DB->prepare("DELETE FROM cached_area_has_parent")->execute();
+			$DB->prepare("UPDATE area_information SET cache_area_has_parent_generated='f'")->execute();
 			
 			$DB->commit();
 		} catch (Exception $e) {
