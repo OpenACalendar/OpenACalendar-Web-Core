bb1407421e193550d9e0ea05881ce533ce0d9b51
James <james@jarofgreen.co.uk>
2014-05-21 10:17:50 +0100

    Unit test to ensure the bug fixed by e1343c72761655d8f029f28cd346db6c601ab5ad doesn't come back - related to #155 & #171

---
+++ core/tests/EventRepositoryBuilderTest.php
@@ -0,0 +1,72 @@
+<?php
+
+use models\UserAccountModel;
+use models\SiteModel;
+use models\AreaModel;
+use repositories\UserAccountRepository;
+use repositories\SiteRepository;
+use repositories\CountryRepository;
+use repositories\AreaRepository;
+use repositories\builders\EventRepositoryBuilder;
+
+
+/**
+ *
+ * @package Core
+ * @link http://ican.openacalendar.org/ OpenACalendar Open Source Software
+ * @license http://ican.openacalendar.org/license.html 3-clause BSD
+ * @copyright (c) 2013-2014, JMB Technology Limited, http://jmbtechnology.co.uk/
+ * @author James Baster <james@jarofgreen.co.uk>
+ */
+
+
+
+
+class EventRepositoryBuilderTest  extends \PHPUnit_Framework_TestCase {
+
+	
+	
+	function testFilterAreaAndIncludeAreaAndIncludeVenue() {
+		$DB = getNewTestDB();
+		addCountriesToTestDB();
+		
+		$countryRepo = new CountryRepository();
+		$areaRepo = new AreaRepository();
+		$userRepo = new UserAccountRepository();
+		$siteRepo = new SiteRepository();
+		
+		$user = new UserAccountModel();
+		$user->setEmail("test@jarofgreen.co.uk");
+		$user->setUsername("test");
+		$user->setPassword("password");
+		
+		$userRepo->create($user);
+		
+		$site = new SiteModel();
+		$site->setTitle("Test");
+		$site->setSlug("test");
+		
+		$siteRepo->create($site, $user, array( $countryRepo->loadByTwoCharCode('GB') ), getSiteQuotaUsedForTesting());
+		
+		$area = new AreaModel();
+		$area->setTitle("test");
+		$area->setDescription("test test");
+		
+		$areaRepo->create($area, null, $site, $countryRepo->loadByTwoCharCode('GB') , $user);
+		$areaRepo->buildCacheAreaHasParent($area);
+		
+		
+		
+		######################## For new just test it doesn't crash, I commited a bug that did crash here
+		
+		$erb = new EventRepositoryBuilder();
+		$erb->setArea($area);
+		$erb->setIncludeVenueInformation(true);
+		$erb->setIncludeAreaInformation(true);
+		$erb->fetchAll();
+		
+		
+	}
+	
+	
+}
