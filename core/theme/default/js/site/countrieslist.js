2f5868acaf9cba5cd23ce4948749a02f408cfcb3
James <james@jarofgreen.co.uk>
2014-06-22 14:40:45 +0100

    Issue #191 - list of countries shows little map and link to maps

---
+++ core/theme/default/js/site/countrieslist.js
@@ -0,0 +1,19 @@
+/**
+ * @package Core
+ * @link http://ican.openacalendar.org/ OpenACalendar Open Source Software - Website
+ * @license http://ican.openacalendar.org/license.html 3-clause BSD
+ * @copyright (c) 2013-2014, JMB Technology Limited, http://jmbtechnology.co.uk/
+ * @author James Baster <james@jarofgreen.co.uk>
+*/
+var map;
+
+$(document).ready(function() {
+	
+	map = L.map('Map');
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);	
+		
+	map.setView([55.952035, -3.196807], 3);
+
+});
