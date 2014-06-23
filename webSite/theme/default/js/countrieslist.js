633171fdadcbd42f514462859829b647a55aac97
James <james@jarofgreen.co.uk>
2014-06-23 22:06:45 +0100

    Closes #191 - break after cols, Zoom map to appropriate bounds

--- webSite/theme/default/js/countrieslist.js
+++ webSite/theme/default/js/countrieslist.js
@@ -13,7 +13,21 @@ $(document).ready(function() {
 		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 		}).addTo(map);	
-		
-	map.setView([55.952035, -3.196807], 3);
-
+	
+	if (countries.length < 20) {
+		var minLat = countries[0].minLat;
+		var minLng = countries[0].minLng;
+		var maxLat = countries[0].maxLat;
+		var maxLng = countries[0].maxLng;
+		for (var i = 0; i < countries.length; i++) {
+			var country = countries[i];
+			if (country.minLat < minLat) minLat  = country.minLat;
+			if (country.minLng < minLng) minLng  = country.minLng;
+			if (country.maxLat > maxLat) maxLat  = country.maxLat;
+			if (country.maxLng > maxLng) maxLng  = country.maxLng;
+		}
+		map.fitBounds([[minLat, minLng],[maxLat, maxLng]]);
+	} else {
+		map.setView([55.952035, -3.196807], 3);
+	}
 });
