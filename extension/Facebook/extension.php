859527ca689fc890bc56dcfc3915ef0e2134ca0e
James <james@jarofgreen.co.uk>
2014-06-30 20:48:03 +0100

    Proper package in comments  -  Issue #182

--- extension/Facebook/extension.php
+++ extension/Facebook/extension.php
@@ -2,7 +2,7 @@
 
 /**
  *
- * @package org.openacalendar.addresscode.gb.opencodepoint
+ * @package org.openacalendar.facebook
  * @link http://ican.openacalendar.org/ OpenACalendar Open Source Software
  * @license http://ican.openacalendar.org/license.html 3-clause BSD
  * @copyright (c) 2013-2014, JMB Technology Limited, http://jmbtechnology.co.uk/
