d9ff1763342d4009a9d9101cb59da20a10d06386
James <james@jarofgreen.co.uk>
2014-08-22 08:55:33 +0100

    better comment to explain stuff

--- build/localConfig.php
+++ build/localConfig.php
@@ -10,7 +10,16 @@
 
 /** If the build scripts are under the APP_ROOT_DIR use this. This is the default. **/
 define('APP_ROOT_DIR',__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR);
-/** If the web roots are under the APP_ROOT_DIR use this. This is the default. **/
+
+
+/** If the web roots are under the APP_ROOT_DIR use this. This is the default.
+ *
+ * If you don't have one of these you can set it's value to null.
+ *
+ * So if you are using Multi Site Mode you can set APP_WEB_SINGLE_SITE_DIR to null.
+ *
+ * So if you are using Single Site Mode you can set APP_WEB_INDEX_DIR, APP_WEB_SITE_DIR and APP_WEB_SYSADMIN_DIR to null.
+ **/
 define('APP_WEB_INDEX_DIR',APP_ROOT_DIR.DIRECTORY_SEPARATOR.'webIndex'.DIRECTORY_SEPARATOR);
 define('APP_WEB_SITE_DIR',APP_ROOT_DIR.DIRECTORY_SEPARATOR.'webSite'.DIRECTORY_SEPARATOR);
 define('APP_WEB_SINGLE_SITE_DIR',APP_ROOT_DIR.DIRECTORY_SEPARATOR.'webSingleSite'.DIRECTORY_SEPARATOR);
