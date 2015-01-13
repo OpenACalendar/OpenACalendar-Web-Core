fc791f77516ca67d2305bff57ac2d3728c234866
James <james@jarofgreen.co.uk>
2015-01-13 12:22:10 +0000

    Closes https://github.com/OpenACalendar/OpenACalendar-Web-Core/issues/276 - when look at imported event with no recurrence, just show the 1 real event, no filters

--- core/php/site/controllers/ImportURLImportedEventController.php
+++ core/php/site/controllers/ImportURLImportedEventController.php
@@ -50,22 +50,40 @@ class ImportURLImportedEventController extends ImportURLController {
 			$app->abort(404, "Import does not exist.");
 		}
 
-		$this->parameters['eventListFilterParams'] = new EventFilterParams();
-		// set some defaults that are different from normal
-		$this->parameters['eventListFilterParams']->setIncludeDeleted(true);
-		$this->parameters['eventListFilterParams']->setFromNow(false);
-		// now carry on ...
-		$this->parameters['eventListFilterParams']->set($_GET);
-		$this->parameters['eventListFilterParams']->getEventRepositoryBuilder()->setSite($app['currentSite']);
-		$this->parameters['eventListFilterParams']->getEventRepositoryBuilder()->setIncludeAreaInformation(true);
-		$this->parameters['eventListFilterParams']->getEventRepositoryBuilder()->setIncludeVenueInformation(true);
-		// Technically we should be able to do this ... but to keep the UI simple, lets not.
-		//if ($app['currentUser'])) {
-		//	$this->parameters['eventListFilterParams']->getEventRepositoryBuilder()->setUserAccount($app['currentUser'], true);
-		//}
-		$this->parameters['eventListFilterParams']->getEventRepositoryBuilder()->setImportedEvent($this->parameters['importedEvent']);
+		if ($this->parameters['importedEvent']->hasReoccurence()) {
 
-		$this->parameters['events'] = $this->parameters['eventListFilterParams']->getEventRepositoryBuilder()->fetchAll();
+			$this->parameters['importedEventReoccurs'] = true;
+
+			$this->parameters['eventListFilterParams'] = new EventFilterParams();
+			// set some defaults that are different from normal
+			$this->parameters['eventListFilterParams']->setIncludeDeleted(true);
+			$this->parameters['eventListFilterParams']->setFromNow(false);
+			// now carry on ...
+			$this->parameters['eventListFilterParams']->set($_GET);
+			$this->parameters['eventListFilterParams']->getEventRepositoryBuilder()->setSite($app['currentSite']);
+			$this->parameters['eventListFilterParams']->getEventRepositoryBuilder()->setIncludeAreaInformation(true);
+			$this->parameters['eventListFilterParams']->getEventRepositoryBuilder()->setIncludeVenueInformation(true);
+			// Technically we should be able to do this ... but to keep the UI simple, lets not.
+			//if ($app['currentUser'])) {
+			//	$this->parameters['eventListFilterParams']->getEventRepositoryBuilder()->setUserAccount($app['currentUser'], true);
+			//}
+			$this->parameters['eventListFilterParams']->getEventRepositoryBuilder()->setImportedEvent($this->parameters['importedEvent']);
+
+			$this->parameters['events'] = $this->parameters['eventListFilterParams']->getEventRepositoryBuilder()->fetchAll();
+
+			$this->parameters['event'] = null;
+
+		} else {
+
+			$this->parameters['importedEventReoccurs'] = false;
+			$this->parameters['eventListFilterParams'] = null;
+			$this->parameters['events'] = null;
+
+			$eventRepo = new EventRepository();
+			$this->parameters['event'] = $eventRepo->loadByImportedEvent($this->parameters['importedEvent']);
+
+		}
+
 
 		return $app['twig']->render('site/importurlimportedevent/index.html.twig',$this->parameters);
 	}
