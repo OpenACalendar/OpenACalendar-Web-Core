72488edc34e96555c19b265035cb7b6c400b2244
James <james@jarofgreen.co.uk>
2015-03-12 07:30:22 +0000

    Another test for https://github.com/OpenACalendar/OpenACalendar-Web-Core/issues/354

--- core/tests/SiteRepositoryBuilderUserInterestedInTest.php
+++ core/tests/SiteRepositoryBuilderUserInterestedInTest.php
@@ -93,4 +93,49 @@ class SiteRepositoryBuilderUserInterestedInTest extends \PHPUnit_Framework_TestC
 
 	}
 
+	function testUserIsUserGroup() {
+
+		$DB = getNewTestDB();
+
+		$user = new UserAccountModel();
+		$user->setEmail("test@jarofgreen.co.uk");
+		$user->setUsername("test");
+		$user->setPassword("password");
+
+		$userTest = new UserAccountModel();
+		$userTest->setEmail("testtest@jarofgreen.co.uk");
+		$userTest->setUsername("testtest");
+		$userTest->setPassword("password");
+
+
+		$userRepo = new UserAccountRepository();
+		$userRepo->create($user);
+		$userRepo->create($userTest);
+
+		$site = new SiteModel();
+		$site->setTitle("Test");
+		$site->setSlug("test");
+
+		$siteRepo = new SiteRepository();
+		$siteRepo->create($site, $user, array(), getSiteQuotaUsedForTesting());
+
+
+		$usrb = new \repositories\builders\UserGroupRepositoryBuilder();
+		$usrb->setSite($site);
+		$userGroups = $usrb->fetchAll();
+		$this->assertTrue(count($userGroups) > 0);
+		$userGroup = $userGroups[0];
+
+		$uiugr = new \repositories\UserGroupRepository();
+		$uiugr->addUserToGroup($userTest, $userGroup);
+
+		// Test user in user group has it
+		$srb = new \repositories\builders\SiteRepositoryBuilder();
+		$srb->setUserInterestedIn($userTest);
+		$sites = $srb->fetchAll();
+
+		$this->assertEquals(1, count($sites));
+
+	}
+
 }
