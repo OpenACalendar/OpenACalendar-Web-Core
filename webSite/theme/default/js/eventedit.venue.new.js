5105118446b7fb0d116543647042fd7191683d9e
James Baster <james@jarofgreen.co.uk>
2015-03-20 18:39:48 +0000

    An earlier commit did not build properly

--- webSite/theme/default/js/eventedit.venue.new.js
+++ webSite/theme/default/js/eventedit.venue.new.js
@@ -2,7 +2,7 @@
  * @package Core
  * @link http://ican.openacalendar.org/ OpenACalendar Open Source Software - Website
  * @license http://ican.openacalendar.org/license.html 3-clause BSD
- * @copyright (c) 2013-2014, JMB Technology Limited, http://jmbtechnology.co.uk/
+ * @copyright (c) 2013-2015, JMB Technology Limited, http://jmbtechnology.co.uk/
  * @author James Baster <james@jarofgreen.co.uk>
 */
 
@@ -11,5 +11,5 @@
 
 
 function clearArea() {
-	$('#AreaFieldsWrapper').html('<input type="hidden" name="fieldChildAreaSlug" value=""><input type="hidden" name="fieldAreaSlug" value=""><input type="hidden" name="fieldArea" value="">');
+	$('#AreaFieldsWrapper').html('<input type="hidden" name="fieldAreaSlug" value="">');
 }
