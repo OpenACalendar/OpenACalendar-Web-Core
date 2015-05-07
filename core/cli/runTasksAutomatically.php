725d1b31bebd6912b0f8dfb82cac9cf8d663dec4
James Baster <james@jarofgreen.co.uk>
2015-05-07 18:21:26 +0100

    If crash in task, log in Monolog. Closes https://github.com/OpenACalendar/OpenACalendar-Web-Core/issues/380

--- core/cli/runTasksAutomatically.php
+++ core/cli/runTasksAutomatically.php
@@ -26,9 +26,14 @@ foreach($app['extensions']->getExtensionsIncludingCore() as $extension) {
 		if ($verbosePrint) {
 			print "    Task ".$task->getTaskId()."\n";
 		}
-		// If we get an exception from runAutomaticallyNowIfShould() we want to crash out so no more tasks are run.
-		// The Exception may have left the DB or other resources in a bad state.
-		$task->runAutomaticallyNowIfShould($verbosePrint);
+		try {
+			$task->runAutomaticallyNowIfShould($verbosePrint);
+		} catch (\Exception $e) {
+			$app['monolog']->addError("Exception Running Tasks Automatically: ".$e->getMessage());
+			// If we get an exception from runAutomaticallyNowIfShould() we want to crash out so no more tasks are run.
+			// The Exception may have left the DB or other resources in a bad state.
+			throw $e;
+		}
 	}
 }
 if ($verbosePrint) {
