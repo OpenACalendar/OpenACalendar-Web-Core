b02bbc5ea15cb3093d8ca4e2bb76e2abaefeba33
James <james@jarofgreen.co.uk>
2017-11-09 17:37:31 +0000

    Vagrant - app and tests boxes switched to official ubuntu sources and improved
      * Clearer directories
      * More RAM for app so you can run composer update
      * For app, add db import if import.sql file exists
      * For app, add aliases

--- Vagrantfile
+++ Vagrantfile
@@ -9,21 +9,21 @@ Vagrant.configure(2) do |config|
 
 	config.vm.define "app" do |normal|
 
-		config.vm.box = "boxcutter/ubuntu1604"
-		config.vm.box_version = "2.0.18"
+		config.vm.box = "ubuntu/xenial64"
+		config.ssh.username = "ubuntu"
 
 		config.vm.network "forwarded_port", guest: 8080, host: 8080
 		config.vm.network "forwarded_port", guest: 8081, host: 8081
 		config.vm.network "forwarded_port", guest: 8082, host: 8082
 
-		config.vm.synced_folder ".", "/vagrant",  :owner=> 'vagrant', :group=>'users', :mount_options => ['dmode=777', 'fmode=777']
+		config.vm.synced_folder ".", "/vagrant",  :owner=> 'ubuntu', :group=>'users', :mount_options => ['dmode=777', 'fmode=777']
 
 		config.vm.provider "virtualbox" do |vb|
 			# Display the VirtualBox GUI when booting the machine
 			vb.gui = false
 
 			# Customize the amount of memory on the VM:
-			vb.memory = "512"
+			vb.memory = "1024"
 		end
 
 		config.vm.provision :shell, path: "vagrant/app/bootstrap.sh"
@@ -32,10 +32,10 @@ Vagrant.configure(2) do |config|
 
 	config.vm.define "tests" do |normal|
 
-		config.vm.box = "boxcutter/ubuntu1604"
-		config.vm.box_version = "2.0.18"
+		config.vm.box = "ubuntu/xenial64"
+		config.ssh.username = "ubuntu"
 
-		config.vm.synced_folder ".", "/vagrant",  :owner=> 'vagrant', :group=>'users', :mount_options => ['dmode=777', 'fmode=777']
+		config.vm.synced_folder ".", "/vagrant",  :owner=> 'ubuntu', :group=>'users', :mount_options => ['dmode=777', 'fmode=777']
 
 		config.vm.provider "virtualbox" do |vb|
 			# Display the VirtualBox GUI when booting the machine
--- vagrant/app/bootstrap.sh
+++ vagrant/app/bootstrap.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 echo "en_GB.UTF-8 UTF-8" >> /etc/locale.gen
 
 locale-gen
@@ -7,25 +9,24 @@ locale-gen
 sudo apt-get update
 sudo apt-get install -y postgresql apache2 php-mbstring php-gd php php-curl php-pgsql git php-intl php-geoip curl zip  phpunit libapache2-mod-php beanstalkd
 
-mkdir /home/vagrant/fileStore
-chown www-data:www-data  /home/vagrant/fileStore
+mkdir /fileStore
+chown www-data:www-data  /fileStore
 
-mkdir /home/vagrant/logs
-chown www-data:www-data  /home/vagrant/logs
+mkdir /logs
+chown www-data:www-data  /logs
 
-mkdir /home/vagrant/bin
-cd /home/vagrant/bin
-wget -q https://getcomposer.org/composer.phar
+mkdir -p /bin
+wget -O /bin/composer.phar -q https://getcomposer.org/composer.phar
 
 cd /vagrant
-php /home/vagrant/bin/composer.phar install
+php /bin/composer.phar install
 
 cp /vagrant/vagrant/app/apache.conf /etc/apache2/sites-enabled/
 cp /vagrant/vagrant/app/config.test.php /vagrant/config.test.php
 cp /vagrant/vagrant/app/config.php /vagrant/config.php
 cp /vagrant/vagrant/app/99-custom.ini /etc/php/7.0/apache2/conf.d/
-cp /vagrant/vagrant/app/test /home/vagrant/test
-chmod a+rx /home/vagrant/test
+cp /vagrant/vagrant/app/test /home/ubuntu/test
+chmod a+rx /home/ubuntu/test
 
 sudo su --login -c "psql -c \"CREATE USER openacalendartest WITH PASSWORD 'testpassword';\"" postgres
 sudo su --login -c "psql -c \"CREATE DATABASE openacalendartest WITH OWNER openacalendartest ENCODING 'UTF8'  LC_COLLATE='en_GB.UTF-8' LC_CTYPE='en_GB.UTF-8'  TEMPLATE=template0 ;\"" postgres
@@ -34,6 +35,11 @@ sudo su --login -c "psql -c \"CREATE USER openacalendar WITH PASSWORD 'password'
 sudo su --login -c "psql -c \"CREATE DATABASE openacalendar WITH OWNER openacalendar ENCODING 'UTF8'  LC_COLLATE='en_GB.UTF-8' LC_CTYPE='en_GB.UTF-8'  TEMPLATE=template0 ;\"" postgres
 
 
+if [ -f /vagrant/import.sql ]
+then
+    export PGPASSWORD=password
+    psql -U openacalendar -hlocalhost  openacalendar -f /vagrant/import.sql
+fi
 
 php /vagrant/core/cli/upgradeDatabase.php
 php /vagrant/core/cli/loadStaticData.php
@@ -43,3 +49,13 @@ chown www-data:www-data  /vagrant/cache/templates.web
 
 a2enmod rewrite
 /etc/init.d/apache2 restart
+
+echo "alias db='psql -U openacalendar openacalendar -hlocalhost'" >> /home/ubuntu/.bashrc
+echo "localhost:5432:openacalendar:openacalendar:password" > /home/ubuntu/.pgpass
+chown ubuntu:ubuntu /home/ubuntu/.pgpass
+chmod 0600 /home/ubuntu/.pgpass
+
+echo "cd /vagrant" >> /home/ubuntu/.bashrc
+echo "alias test='/home/ubuntu/test'" >> /home/ubuntu/.bashrc
+echo "alias composer-update='cd /vagrant && php /bin/composer.phar update'" >> /home/ubuntu/.bashrc
+echo "alias composer-install='cd /vagrant && php /bin/composer.phar install'" >> /home/ubuntu/.bashrc
