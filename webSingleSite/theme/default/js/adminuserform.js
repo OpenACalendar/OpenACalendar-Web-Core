368402bd7fd32aa239911ff3def67b64c6a756d5
James <james@jarofgreen.co.uk>
2017-11-16 13:41:58 +0000

    /core/theme/default/js/site/adminuserform.js not used, remove! closes https://github.com/OpenACalendar/OpenACalendar-Web-Core/issues/761 [skip ci]

--- webSingleSite/theme/default/js/adminuserform.js
+++ webSingleSite/theme/default/js/adminuserform.js
@@ -1,33 +1,3 @@
 /**
- * @package Core
- * @link http://ican.openacalendar.org/ OpenACalendar Open Source Software - Website
- * @license http://ican.openacalendar.org/license.html 3-clause BSD
- * @copyright (c) JMB Technology Limited, http://jmbtechnology.co.uk/
- * @author James Baster <james@jarofgreen.co.uk>
+ * This file removed.
 */
-$(document).ready(function() {
-
-	$('#isAllUsersEditorsWrapper input, #isRequestAccessAllowedWrapper input').change(function() {  
-		changeForm();
-	});
-	
-	changeForm();
-});
-
-
-function changeForm() {
-	var isAllUsersEditors = $('#isAllUsersEditorsWrapper input').is(':checked');
-	if (isAllUsersEditors) {
-		$('#isRequestAccessAllowedWrapper').hide();
-		$('#requestAccessQuestionWrapper').hide();
-	} else {
-		$('#isRequestAccessAllowedWrapper').show();
-		var isRequestAccessAllowed = $('#isRequestAccessAllowedWrapper input').is(':checked');
-		if (isRequestAccessAllowed) {
-			$('#requestAccessQuestionWrapper').show();
-		} else {
-			$('#requestAccessQuestionWrapper').hide();
-		}
-	}
-}
-
